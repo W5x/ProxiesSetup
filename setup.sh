d76a21816dec8bf808181f795209fdf61bcb97f7
Yasser <official.sylar@gmail.com>
2024-04-13 15:15:56 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -138,14 +138,13 @@ echo "--------------------------------------------------------------------------
 echo "--------------------------------------------------------------------------------------------------"
 GREEN='\033[0;32m'
 CYAN='\033[0;36m'
+RED='\033[0;31m'
+Yellow='\033[0;33m'
 NC='\033[0m'
 echo -e "${NC}"
 
-echo -e "${GREEN}Browser Format${NC}"
-echo -e "${GREEN}HTTP Proxy   : http://$external_ip:3128:$username:$password${NC}"
-echo -e "${GREEN}SOCKS5 Proxy : socks5://$external_ip:9098:$username:$password${NC}"
-echo 
-echo -e "${GREEN}Bot Format${NC}"
-echo -e "${GREEN}HTTP Proxy   : http://$username:$password@$external_ip:3128${NC}"
-echo -e "${GREEN}SOCKS5 Proxy : socks5://$username:$password@$external_ip:9098${NC}"
+echo -e "${GREEN}HTTP Browser   : http://$external_ip:3128:$username:$password${NC}"
+echo -e "${GREEN}SOCKS5 Browser : socks5://$external_ip:9098:$username:$password${NC}"
+echo -e "${RED}HTTP Bot         : http://$username:$password@$external_ip:3128${NC}"
+echo -e "${RED}SOCKS5 Bot       : socks5://$username:$password@$external_ip:9098${NC}"
 echo
